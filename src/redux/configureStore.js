51189d29d117b073870a78c909e83ab8e1efbb8d
Abu Raihan <raihan2bd.official@gmail.com>
2023-01-23 14:13:51 +0600

    Join multiple reducer inside configureStore

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/books';
+import categoriesReducer from './categories/categories';
 
 const store = configureStore({
-  reducer: booksReducer,
+  reducer: { books: booksReducer, categories: categoriesReducer },
 });
 
 export default store;
